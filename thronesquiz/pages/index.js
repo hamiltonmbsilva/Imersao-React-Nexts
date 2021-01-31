d48cea2833712af6c1d9b281fc6b5ef79c329f09
hamiltonmbsilva <hamiltonmb.silva@gmail.com>
2021-01-31 10:39:00 -0300

    Criando as div para colocar os quiz

--- thronesquiz/pages/index.js
+++ thronesquiz/pages/index.js
@@ -8,10 +8,51 @@ const BackgroundImage = styled.div`
   background-position: center;
 `;
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4CAF50;
+  background-color: #1C1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p{
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
 export default function Home() {
   return (
     <BackgroundImage>
-        asasas
+      <QuizContainer>
+         <Widget>
+            <h1>Game of Thrones</h1>
+            <p>Texto complementar...</p>
+         </Widget>
+         <Widget>
+            <h1>Quiz da Galera</h1>
+            <p>Texto complementar...</p>
+         </Widget>
+      </QuizContainer>       
     </BackgroundImage>
   );
 }
